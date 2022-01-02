9c2333a62fe31552e6cc7763ef7777bbf2b3baab
pearz <1095971161@qq.com>
2022-01-02 15:34:28 +0800

    Initial commit

---
+++ spring5/spring5_demo1/src/main/java/com/pearz/spring5/User.java
@@ -0,0 +1,13 @@
+package com.pearz.spring5;
+
+/**
+ * @Description
+ * @Author pearz
+ * @Email zhaihonghao317@163.com
+ * @Date 19:55 2022/1/1
+ */
+public class User {
+    public void add() {
+        System.out.println("add......");
+    }
+}
---
+++ spring5/spring5_demo1/src/main/java/com/pearz/spring5/test/Spring5Test.java
@@ -0,0 +1,22 @@
+package com.pearz.spring5.test;
+
+import com.pearz.spring5.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author pearz
+ */
+public class Spring5Test {
+
+    @Test
+    public void testAdd() {
+        //1、加载spring配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        //2、获取配置创建的对象
+        User user = context.getBean("user", User.class);
+        System.out.println(user);
+        user.add();
+    }
+}
