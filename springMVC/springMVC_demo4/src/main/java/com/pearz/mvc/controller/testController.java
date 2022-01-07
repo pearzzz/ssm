6bc31fb5bc2285faac5eb1160ff13fd4a4c09097
pearz <1095971161@qq.com>
2022-01-07 21:36:54 +0800

    SpringMVC 6.view 学完

--- springMVC/springMVC_demo4/src/main/java/com/pearz/mvc/controller/testController.java
+++ springMVC/springMVC_demo4/src/main/java/com/pearz/mvc/controller/testController.java
@@ -13,8 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class testController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "index";
+    //@RequestMapping("/")
+    //public String index() {
+    //    return "index";
+    //}
+
+    @RequestMapping("/test_view")
+    public String testView() {
+        return "test_view";
     }
 }
---
+++ springMVC/springMVC_demo4/src/main/java/com/pearz/mvc/controller/viewController.java
@@ -0,0 +1,28 @@
+package com.pearz.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description
+ * @Author pearz
+ * @Email zhaihonghao317@163.com
+ * @Date 19:05 2022/1/7
+ */
+@Controller
+public class viewController {
+
+    @RequestMapping("/testThymeleafView")
+    public String testThymeleafView() {
+        return "success";
+    }
+
+    @RequestMapping("/testForward")
+    public String testForward() {
+        return "forward:/testThymeleafView";
+    }
+    @RequestMapping("/testRedirect")
+    public String testRedirect() {
+        return "redirect:/testThymeleafView";
+    }
+}
