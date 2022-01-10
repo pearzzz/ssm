80f2170fd0fd37369b3a6bb3423213ba5f4f28cd
pearz <1095971161@qq.com>
2022-01-10 23:20:08 +0800

    SpringMVC 11.异常处理 学完

---
+++ springMVC/springMVC_demo5/src/main/java/com/pearz/mvc/controller/ExceptionController.java
@@ -0,0 +1,21 @@
+package com.pearz.mvc.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * @Description
+ * @Author pearz
+ * @Email zhaihonghao317@163.com
+ * @Date 23:17 2022/1/10
+ */
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(value = {ArithmeticException.class, NullPointerException.class})
+    public String testException(Exception ex, Model model) {
+        model.addAttribute("ex", ex);
+        return "error";
+    }
+}
